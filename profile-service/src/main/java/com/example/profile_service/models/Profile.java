3b1b222b6cdb78a99990d4ecf548d3f49dbbdd95
uriGrif <urielgrif@gmail.com>
2025-02-19 17:44:41 -0300

    agrego fechas de creacion y modificacion a db de profile

--- profile-service/src/main/java/com/example/profile_service/models/Profile.java
+++ profile-service/src/main/java/com/example/profile_service/models/Profile.java
@@ -3,8 +3,13 @@ package com.example.profile_service.models;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Data;
+import java.util.Date;
 
 @Entity
 @Table(name = "profile")
@@ -22,4 +27,22 @@ public class Profile {
 
   @Column(columnDefinition = "TEXT")
   private String description;
+
+  @Column(name = "created_at")
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date createdAt;
+
+  @Column(name = "updated_at")
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date updatedAt;
+
+  @PrePersist
+  public void setCreatedTime() {
+    this.createdAt = new Date();
+  }
+
+  @PreUpdate
+  public void setUpdatedTime() {
+    this.updatedAt = new Date();
+  }
 }
